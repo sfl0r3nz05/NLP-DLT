736d1c49e1a75ecc16d0c47641b195f2f1cd6c17
Ubuntu <ubuntu@ip-172-31-16-5.ec2.internal>
2021-07-07 15:07:12 +0000

    07.07.12.2

--- nlp/library/src/library/UpdateFileV2.py
+++ nlp/library/src/library/UpdateFileV2.py
@@ -5,7 +5,7 @@ def updateFile(filePath, key1, index, key2, entity):
     with open(filePath, 'r') as ra:
         data = json.load(ra)
         if key2 != "0":
-            data[key1]['uid'] = str(uuid.uuid1())
+            data[key1]['uid'] = str(uuid.uuid1())  # make a UUID based on the host ID and current time (https://docs.python.org/3/library/uuid.html#example)
             data[key1][key2] = entity
     with open(filePath, 'w') as ra:
         json.dump(data, ra, indent=4)
