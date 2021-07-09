dad0c391a6b172e85e1ee2ae932e56569f07d25d
Ubuntu <ubuntu@ip-172-31-16-5.ec2.internal>
2021-07-09 12:39:26 +0000

    09.07.21.3

--- nlp/library/src/library/ManageEntities/DateFinder.py
+++ nlp/library/src/library/ManageEntities/DateFinder.py
@@ -1,10 +1,10 @@
-"""
-The dateFilter method receives as input a list of entities following the format returned by amazon comprehend 
-(e.g.:{'BeginOffset': 28, 'EndOffset': 38, 'Score': 0. 9668462872505188, 'Text': '29/05/2017', 'Type': 'DATE'}) 
-and returns a list of dictionaries where each dictionary contains the date, the number of times that date is repeated (frequency) 
-and the maximum score associated with that date (e.g.: {'date': '29/05/2017', 'score': 0.9966247081756592, 'count': 28})
-"""
 def dateFilter(entitiesList):
+    """
+    The dateFilter method receives as input a list of entities following the format returned by amazon comprehend 
+    (e.g.:{'BeginOffset': 28, 'EndOffset': 38, 'Score': 0. 9668462872505188, 'Text': '29/05/2017', 'Type': 'DATE'}) 
+    and returns a list of dictionaries where each dictionary contains the date, the number of times that date is repeated (frequency) 
+    and the maximum score associated with that date (e.g.: {'date': '29/05/2017', 'score': 0.9966247081756592, 'count': 28})
+    """
     date_list = []
     for entity in entitiesList:
         if entity["Type"] == 'DATE':
@@ -20,12 +20,13 @@ def dateFilter(entitiesList):
             if flag == 0:
                 date_list.append({"date":str(entity["Text"]), "score":str(entity["Score"]), "freq":1})
     return date_list
-"""
-The dateSelection method receives as input a list of dictionaries (e.g.: {'date': '29/05/2017', 'score': 0.9966247081756592, 'count': 28}) 
-and returns a string (e.g., '29/05/2017') which represents the selected date. To do this, it selects the highest score and the highest frequency, if both match, 
-the date associated with these two is returned, otherwise the frequency is given priority.
-"""
+
 def dateSelection(date_list):
+    """
+    The dateSelection method receives as input a list of dictionaries (e.g.: {'date': '29/05/2017', 'score': 0.9966247081756592, 'count': 28}) 
+    and returns a string (e.g., '29/05/2017') which represents the selected date. To do this, it selects the highest score and the highest frequency, if both match, 
+    the date associated with these two is returned, otherwise the frequency is given priority.
+    """
     temp1 = 0
     temp2 = 0
     temp3 = ""
@@ -41,10 +42,11 @@ def dateSelection(date_list):
         return temp3
     else:
         return temp4
-"""
-The dateFinder method calls and integrates both methods dateFilter and dateSelection.
-"""
+
 def dateFinder(entitiesList):
+    """
+    The dateFinder method calls and integrates both methods dateFilter and dateSelection.
+    """
     date_list = dateFilter(entitiesList)
     date = dateSelection(date_list)
     return date
