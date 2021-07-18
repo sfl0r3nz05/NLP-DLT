3e4aa4f57227196931c92fe31dc617f47d1fdf5b
Ubuntu <ubuntu@ip-172-31-16-5.ec2.internal>
2021-07-18 15:39:27 +0000

    18.07.21.4

--- nlp/library/src/library/Similarity/FindSimilarity.py
+++ nlp/library/src/library/Similarity/FindSimilarity.py
@@ -1,6 +1,21 @@
 import json
-
 list_sub_art_tagged = []
+list_similarity = []
+new_list = []
+obj_similarity = {'number':'','similarity':''}
+
+def setSimilarity(list_sub_articles, list_similarity):
+    for similarityX in list_similarity:
+        for articleX in list_sub_articles:
+            #if(similarityX['number'] == articleX['content']['number']):
+                #print('hereeee')
+
+def lenMng(num):
+    if(len(num) == 4):
+        n = num[2:]
+    else:
+        n = num[1:]
+    return n
 
 def get_jaccard_sim(str1, str2):
     a = set(str1.split()) 
@@ -13,9 +28,18 @@ def matchSubArticles(data, article):
     article_content = article['content']
     for number1 in data_content:
         num1 = number1['number']
+        cont1 = number1['content']
+        n1 = lenMng(num1)
         for number2 in article_content:
             num2 = number2['number']
-            if():
+            cont2 = number2['content']
+            n2 = lenMng(num2)
+            if(n1 == n2):
+                percent = get_jaccard_sim(cont1, cont2)
+                obj_similarity['number'] = num2
+                obj_similarity['similarity'] = percent
+                dictionary_copy = obj_similarity.copy()
+                list_similarity.append(dictionary_copy)
 
 def findSimilarity(list_sub_articles, articlesTemplate):
     with open(articlesTemplate, 'r') as ra:
@@ -26,4 +50,5 @@ def findSimilarity(list_sub_articles, articlesTemplate):
             for article in list_sub_articles:
                 if(article['name'] in name_list):
                     matchSubArticles(data, article)
-    return list_sub_art_tagged
+    setSimilarity(list_sub_articles, list_similarity)
+    return new_list
