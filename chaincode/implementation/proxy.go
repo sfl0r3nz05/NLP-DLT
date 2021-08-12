8fcfc2bc1e15299790188fa5312210087b6d9291
Ubuntu <ubuntu@ip-172-31-16-137.ec2.internal>
2021-08-12 22:17:29 +0000

    12.08.9

--- chaincode/implementation/proxy.go
+++ chaincode/implementation/proxy.go
@@ -19,13 +19,88 @@ func (cc *Chaincode) Init(APIstub shim.ChaincodeStubInterface) sc.Response {
 func (cc *Chaincode) Invoke(stub shim.ChaincodeStubInterface) sc.Response {
     function, args := stub.GetFunctionAndParameters()
 
-    if function == "get" {
-        cc.get(stub, args)
-    }
+    if function == "addOrg" {
+        cc.registerOrg(stub, args)
+    } else if function == "proposeAgreementInitiation" {
+		cc.startAgreement(stub, args)
+	} else if function == "acceptAgreementInitiation" {
+		cc.confirmAgreement(stub, args)
+	} else if function == "proposeAddArticle" {
+		cc.addArticle(stub, args)
+	} else if function == "acceptAddArticle" {
+		cc.acceptArticle(stub, args)
+	} else if function == "denyAddArticle" {
+		cc.denyArticle(stub, args)
+	} else if function == "proposeUpdateArticle" {
+		cc.updateArticle(stub, args)
+	} else if function == "acceptUpdateArticle" {
+		cc.acceptUpdArticle(stub, args)
+	} else if function == "denyUpdateArticle" {
+		cc.denyUpdArticle(stub, args)
+	} else if function == "proposeDeleteArticle" {
+		cc.delArticle(stub, args)
+	} else if function == "acceptDeleteArticle" {
+		cc.acceptDelArticle(stub, args)
+	} else if function == "denyDeleteArticle" {
+		cc.denyDelArticle(stub, args)
+	} else if function == "reachAgreement" {
+		cc.acceptReachAgree(stub, args)
+	} else if function == "acceptReachAgreement" {
+		cc.confirmAchieRA(stub, args)
+	} else if function == "querySingleArticle" {
+		cc.queryArticle(stub, args)
+	} else if function == "queryAllArticles" {
+		cc.queryRAarticles(stub, args)
+	}
 	return shim.Success([]byte("OK"))
 }
 
-func (cc *Chaincode) get(stub shim.ChaincodeStubInterface, args []string) (string, error){
+func (cc *Chaincode) registerOrg(stub shim.ChaincodeStubInterface, args []string) (string, error){
+	return "" , errors.New(ERRORWrongNumberArgs)
+}
+func (cc *Chaincode) startAgreement(stub shim.ChaincodeStubInterface, args []string) (string, error){
+	return "" , errors.New(ERRORWrongNumberArgs)
+}
+func (cc *Chaincode) confirmAgreement(stub shim.ChaincodeStubInterface, args []string) (string, error){
+	return "" , errors.New(ERRORWrongNumberArgs)
+}
+func (cc *Chaincode) addArticle(stub shim.ChaincodeStubInterface, args []string) (string, error){
+	return "" , errors.New(ERRORWrongNumberArgs)
+}
+func (cc *Chaincode) acceptArticle(stub shim.ChaincodeStubInterface, args []string) (string, error){
+	return "" , errors.New(ERRORWrongNumberArgs)
+}
+func (cc *Chaincode) denyArticle(stub shim.ChaincodeStubInterface, args []string) (string, error){
+	return "" , errors.New(ERRORWrongNumberArgs)
+}
+func (cc *Chaincode) updateArticle(stub shim.ChaincodeStubInterface, args []string) (string, error){
+	return "" , errors.New(ERRORWrongNumberArgs)
+}
+func (cc *Chaincode) acceptUpdArticle(stub shim.ChaincodeStubInterface, args []string) (string, error){
+	return "" , errors.New(ERRORWrongNumberArgs)
+}
+func (cc *Chaincode) denyUpdArticle(stub shim.ChaincodeStubInterface, args []string) (string, error){
+	return "" , errors.New(ERRORWrongNumberArgs)
+}
+func (cc *Chaincode) delArticle(stub shim.ChaincodeStubInterface, args []string) (string, error){
+	return "" , errors.New(ERRORWrongNumberArgs)
+}
+func (cc *Chaincode) acceptDelArticle(stub shim.ChaincodeStubInterface, args []string) (string, error){
+	return "" , errors.New(ERRORWrongNumberArgs)
+}
+func (cc *Chaincode) denyDelArticle(stub shim.ChaincodeStubInterface, args []string) (string, error){
+	return "" , errors.New(ERRORWrongNumberArgs)
+}
+func (cc *Chaincode) acceptReachAgree(stub shim.ChaincodeStubInterface, args []string) (string, error){
+	return "" , errors.New(ERRORWrongNumberArgs)
+}
+func (cc *Chaincode) confirmAchieRA(stub shim.ChaincodeStubInterface, args []string) (string, error){
+	return "" , errors.New(ERRORWrongNumberArgs)
+}
+func (cc *Chaincode) queryArticle(stub shim.ChaincodeStubInterface, args []string) (string, error){
+	return "" , errors.New(ERRORWrongNumberArgs)
+}
+func (cc *Chaincode) queryRAarticles(stub shim.ChaincodeStubInterface, args []string) (string, error){
 	return "" , errors.New(ERRORWrongNumberArgs)
 }
 
