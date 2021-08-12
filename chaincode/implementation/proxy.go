2afd4db101079576bd5aa155541c8f362181bcec
Ubuntu <ubuntu@ip-172-31-16-137.ec2.internal>
2021-08-12 19:49:14 +0000

    12.08.1

--- chaincode/implementation/proxy.go
+++ chaincode/implementation/proxy.go
@@ -1,40 +1,37 @@
 package main
 
 import (
-	"fmt"
+    "fmt"
 	"errors"
-	"strconv"
-	pb "github.com/hyperledger/fabric/protos/peer"
-	"github.com/hyperledger/fabric/core/chaincode/shim"
+	log "github.com/sirupsen/logrus"
+	"github.com/hyperledger/fabric-chaincode-go/shim"
+	sc "github.com/hyperledger/fabric-protos-go/peer"
 )
 
-// Chaincode example simple Chaincode implementation
 type Chaincode struct {
 }
 
-var CHANNEL_ENV string
-var outC string
-var TxID string
-var sum string
-var out string
-
-func (cc *ChainCode) Init(APIstub shim.ChaincodeStubInterface) sc.Response {
-	return shim.Success(m.OK)
+func (cc *Chaincode) Init(APIstub shim.ChaincodeStubInterface) sc.Response {
+	log.Info("DEBUG")
+    return shim.Success([]byte("OK"))
 }
 
-func (cc *Chaincode) Invoke(stub shim.ChaincodeStubInterface) pb.Response {
-	function, args := stub.GetFunctionAndParameters()
+func (cc *Chaincode) Invoke(stub shim.ChaincodeStubInterface) sc.Response {
+    function, args := stub.GetFunctionAndParameters()
 
-	if function == "get" {
-		cc.get(stub, args)			// Make payment of X units from A to B
-	} else if function == "set" {
-		cc.set(stub, args)			// Query an entity from its state
-	}
+    if function == "get" {
+        cc.get(stub, args)
+    }
+	return shim.Success([]byte("OK"))
+}
+
+func (cc *Chaincode) get(stub shim.ChaincodeStubInterface, args []string) (string, error){
+	return "" , errors.New(ERRORWrongNumberArgs)
 }
 
 func main() {
-	err := shim.Start(new(Chaincode))
-	if err != nil {
-		fmt.Printf("Error starting Simple chaincode: %s", err)
-	}
+    err := shim.Start(new(Chaincode))
+    if err != nil {
+        fmt.Printf("Error starting Simple chaincode: %s", err)
+    }
 }
