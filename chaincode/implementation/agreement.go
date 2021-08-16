ab928155f76a461d4adfbf4a4f0ac06e171dae38
Ubuntu <ubuntu@ip-172-31-16-137.ec2.internal>
2021-08-16 12:16:12 +0000

    16.08.21.22

--- chaincode/implementation/agreement.go
+++ chaincode/implementation/agreement.go
@@ -1,1 +1,44 @@
 package main
+import (
+    "errors"
+    "encoding/json"
+    log "github.com/sirupsen/logrus"
+    "github.com/hyperledger/fabric-chaincode-go/shim"
+)
+
+func (cc *Chaincode) recordRAJson(stub shim.ChaincodeStubInterface, uuid string, jsonRA string) (error){
+
+    var jsonRAgreement JSONROAMINGAGREEMENT    
+    json.Unmarshal([]byte(jsonRA), &jsonRAgreement)
+
+    idBytes, err := json.Marshal(jsonRAgreement)
+    if err != nil {
+        log.Errorf("[%s][%s][recordRAJson] Error parsing: %v", CHANNEL_ENV, ERRORParsingRA, err.Error())
+        return errors.New(ERRORParsingRA + err.Error())
+    }
+
+    err = stub.PutState(uuid, idBytes) // PuState of Client (Organization) Identity and Organtization struct
+    if err != nil {
+        log.Errorf("[%s][%s][recordRAJson] Error storing: %v", CHANNEL_ENV, ERRORStoringRA, err.Error())
+        return errors.New(ERRORStoringRA + err.Error())
+    }
+
+    return nil
+}
+
+
+func (cc *Chaincode) setAgreement(stub shim.ChaincodeStubInterface, org1_id string, org2_id string, uuid string, status string) (string, error){
+    RA, err := json.Marshal(ROAMINGAGREEMNT{UUID: uuid, ORG1_ID: org1_id, ORG2_ID: org2_id, STATUS: status})
+    if err != nil {
+        log.Errorf("[%s][%s] Error parsing: %v", CHANNEL_ENV, ERRORParsing, err.Error())
+        return "", errors.New(ERRORParsingRA + err.Error())
+    }
+    raid := uuidgen()
+    err = stub.PutState(raid, RA) // PuState of Client (Organization) Identity and Organtization struct
+    if err != nil {
+        log.Errorf("[%s][%s][setAgreement] Error storing: %v", CHANNEL_ENV, ERRORStoringRA, err.Error())
+        return "", errors.New(ERRORStoringRA + err.Error())
+    }
+
+    return raid, nil
+}
--- chaincode/implementation/models.go
+++ chaincode/implementation/models.go
@@ -2,97 +2,119 @@ package main
 
 // Compliant with Official Document IR.21 - GSM Association Roaming Database, Structure and Updating Procedures
 type Organization struct {
-	mno_name string 	 			`json:"mno_name,omitempty"`
-	mno_country string      		`json:"mno_country,omitempty"`
-	mno_network Network  			`json:"mno_network,omitempty"` // issuer's DID
+	mno_name string 	 				`json:"mno_name,omitempty"`
+	mno_country string      			`json:"mno_country,omitempty"`
+	mno_network NETWORK  				`json:"mno_network,omitempty"` // issuer's DID
 }
 
 // Network
-type Network struct {
-	TADIG int            			`json:"tadig,omitempty"`
-	nwk_info string 	 			`json:"nwk_info,omitempty"`
-	db_info DBINFO  	 			`json:"db_info,omitempty"` // issuer's DID
+type NETWORK struct {
+	tadig int            				`json:"tadig,omitempty"`
+	nwk_info string 	 				`json:"nwk_info,omitempty"`
+	db_info DBINFO  	 				`json:"db_info,omitempty"` // issuer's DID
 }
 
 // Network
 type DBINFO struct {
-	technology string    			`json:"technology,omitempty"`
-	frequency string 	 			`json:"frequency,omitempty"`
-	country_initials string  		`json:"country_initials,omitempty"`
-	mobile_network_name string   	`json:"mobile_network_name,omitempty"`
-	network_colour_code string   	`json:"network_colour_code,omitempty"`
-	sim_header_info string   		`json:"sim_header_info,omitempty"`
+	technology string    				`json:"technology,omitempty"`
+	frequency string 	 				`json:"frequency,omitempty"`
+	country_initials string  			`json:"country_initials,omitempty"`
+	mobile_network_name string   		`json:"mobile_network_name,omitempty"`
+	network_colour_code string   		`json:"network_colour_code,omitempty"`
+	sim_header_info string   			`json:"sim_header_info,omitempty"`
 }
 
 // Struct used to return
 type UUIDRAID struct {
-	UUID string `json:"uuid,omitempty"`
-	RAID string `json:"raid,omitempty"`
+	UUID string 						`json:"uuid,omitempty"`
+	RAID string 						`json:"raid,omitempty"`
+}
+
+// Struct used to return
+type ROAMINGAGREEMNT struct {
+	UUID string 						`json:"uuid,omitempty"`
+	ORG1_ID string 						`json:"org1_id,omitempty"`
+	ORG2_ID string 						`json:"org2_id,omitempty"`
+	STATUS string						`json:"status,omitempty"`
+}
+
+type JSONROAMINGAGREEMENT struct {
+	uuid	string						`json:"uuid"` // name for the event
+	document_name	string				`json:"document_name"` // name for the event
+	articles	[]string				`json:"articles"` // name for the event
+}
+
+type ARTICLE struct {
+	id	string							`json:"id"` // name for the event
+	variables	[]string				`json:"variables"` // name for the event
+	variations	[]string				`json:"variations"` // name for the event
 }
 
 // Event to handle events in HF
 type Event struct {
-	ChaincodeId	string				`json:"chaincodeid"` // name for the event
-	Txid	string					`json:"txid"` // name for the event
-	EventName string 	 			`json:"eventName"` // name for the event
-	Payload   []byte 	 			`json:"payload"`   // payload for the
+	ChaincodeId	string					`json:"chaincodeid"` // name for the event
+	Txid	string						`json:"txid"` // name for the event
+	EventName string 	 				`json:"eventName"` // name for the event
+	Payload   []byte 	 				`json:"payload"`   // payload for the
 }
 
 // Error responses
 // ERROR_XXX occurs when XXX
 const (
-	ERRORWrongNumberArgs  		= `Wrong number of arguments. Expecting a JSON with token information.`
-	ERRORParsingData      		= `Error parsing data `
-	ERRORPutState         		= `Failed to store data in the ledger.	`
-	ERRORGetState         		= `Failed to get data from the ledger. `
-	ERRORDelState         		= `Failed to delete data from the ledger. `
-	ERRORChaincodeCall    		= `Error calling chaincode`
-	ERRORGetService       		= `Error getting service`
-	ERRORUpdService       		= `Error updating service`
-	ERRORServiceNotExists 		= `Error The service doesn't exist`
-	ERRORCreatingService  		= `Error storing service`
-	ERRORParsingService   		= `Error parsing service`
-	ERRORServiceExists    		= `The service already exists in registry`
-	ERRORDidMissing       		= `Error calling service, no service DID Specified`
-	ERRORStoringIdentity  		= `Error storing identity`
-	ERRORUpdatingID       		= `Error updating identity in ledger`
-	ERRORGetID            		= `Error getting identity`
-	ERRORVerID            		= `Error verification unauthorized, the did provided has not access`
-	ERRORRevID            		= `Error revocation unauthorized, the did provided has not access`
-	ERRORVerSign          		= `Error verifying signature`
-	ERRORRevSign          		= `Error revoking signature`
-	ERRORRevoke           		= `Error revoking Unauthorized, the did provided cannot revoke the identity`
-	ERRORnotID            		= `Error the identity does not exist`
-	ERRORParsingID        		= `Error parsing identity`
-	ERRORRevokeLedger     		= `Error deleting from ledger`
-	ERRORIDExists         		= `Error the identity already exists`
-	ERRORUserAccess       		= `Error user has not access`
-	ERRORParseJWS         		= `Error parsing into JWS`
-	ERRORParseX509        		= `Error parsing into X509`
-	ERRORBase64           		= `Error decoding into base64`
-	ERRORVerifying        		= `Error verifying signature `
-	ERRORRecoveringOrg	  		= `Error recovering organization`
-	IDREGISTER            		= `Identity to register`
-	IDREGISTRY            		= `Identity already registered`
-	ERRORUserID	          		= `Error user has not identity`
-	ERRORStoringOrg  	  		= `Error storing org`
-	ERRORParsingOrg  	  		= `Error when parsing org to byte`
-	ERRORAgreement  	  		= `Error when agreement is created`
-	ERRORParsing	  	  		= `Error when parsing byte`
-	ERRORRecoverIdentity  		= `Error Identity is recovered`
-	ERRORAddArticle				= `Error adding article`
-	ERRORAcceptAddArticle		= `Error accepting add article`
-	ERRORDenyAddArticle			= `Error denying add article`
-	ERRORUpdateArticle			= `Error updating article`
-	ERRORAcceptUpdateArticle	= `Error accepting update article`
-	ERRORDenyUpdateArticle		= `Error denying update article`
-	ERRORDeleteArticle			= `Error deleting article`
-	ERRORAcceptDeleteArticle	= `Error accepting delete article`
-	ERRORDenyDeleteArticle		= `Error denying delete article`
-	ERRORReachAgreement			= `Error: Agreement not reached`
-	ERRORAcceptAgreement		= `Error: Agreement not accepted`
-	ERRORQuerySingleArticle 	= `Error recovering single article`
-	ERRORQueryAllArticles		= `Error recovering all articles`
-	ERROREventName				= `Error missing Event Name`
-	ERROREventEmit				= `Failed to emit Event`
+	ERRORWrongNumberArgs  				= `Wrong number of arguments. Expecting a JSON with token information.`
+	ERRORParsingData      				= `Error parsing data `
+	ERRORPutState         				= `Failed to store data in the ledger.	`
+	ERRORGetState         				= `Failed to get data from the ledger. `
+	ERRORDelState         				= `Failed to delete data from the ledger. `
+	ERRORChaincodeCall    				= `Error calling chaincode`
+	ERRORGetService       				= `Error getting service`
+	ERRORUpdService       				= `Error updating service`
+	ERRORServiceNotExists 				= `Error The service doesn't exist`
+	ERRORCreatingService  				= `Error storing service`
+	ERRORParsingService   				= `Error parsing service`
+	ERRORServiceExists    				= `The service already exists in registry`
+	ERRORDidMissing       				= `Error calling service, no service DID Specified`
+	ERRORStoringIdentity  				= `Error storing identity`
+	ERRORUpdatingID       				= `Error updating identity in ledger`
+	ERRORGetID            				= `Error getting identity`
+	ERRORVerID            				= `Error verification unauthorized, the did provided has not access`
+	ERRORRevID            				= `Error revocation unauthorized, the did provided has not access`
+	ERRORVerSign          				= `Error verifying signature`
+	ERRORRevSign          				= `Error revoking signature`
+	ERRORRevoke           				= `Error revoking Unauthorized, the did provided cannot revoke the identity`
+	ERRORnotID            				= `Error the identity does not exist`
+	ERRORParsingID        				= `Error parsing identity`
+	ERRORRevokeLedger     				= `Error deleting from ledger`
+	ERRORIDExists         				= `Error the identity already exists`
+	ERRORUserAccess       				= `Error user has not access`
+	ERRORParseJWS         				= `Error parsing into JWS`
+	ERRORParseX509        				= `Error parsing into X509`
+	ERRORBase64           				= `Error decoding into base64`
+	ERRORVerifying        				= `Error verifying signature `
+	ERRORRecoveringOrg	  				= `Error recovering organization`
+	IDREGISTER            				= `Identity to register`
+	IDREGISTRY            				= `Identity already registered`
+	ERRORUserID	          				= `Error user has not identity`
+	ERRORStoringOrg  	  				= `Error storing org`
+	ERRORParsingOrg  	  				= `Error when parsing org to byte`
+	ERRORAgreement  	  				= `Error when agreement is created`
+	ERRORParsing	  	  				= `Error when parsing byte`
+	ERRORRecoverIdentity  				= `Error Identity is recovered`
+	ERRORAddArticle						= `Error adding article`
+	ERRORAcceptAddArticle				= `Error accepting add article`
+	ERRORDenyAddArticle					= `Error denying add article`
+	ERRORUpdateArticle					= `Error updating article`
+	ERRORAcceptUpdateArticle			= `Error accepting update article`
+	ERRORDenyUpdateArticle				= `Error denying update article`
+	ERRORDeleteArticle					= `Error deleting article`
+	ERRORAcceptDeleteArticle			= `Error accepting delete article`
+	ERRORDenyDeleteArticle				= `Error denying delete article`
+	ERRORReachAgreement					= `Error: Agreement not reached`
+	ERRORAcceptAgreement				= `Error: Agreement not accepted`
+	ERRORQuerySingleArticle 			= `Error recovering single article`
+	ERRORQueryAllArticles				= `Error recovering all articles`
+	ERROREventName						= `Error missing Event Name`
+	ERROREventEmit						= `Failed to emit Event`
+	ERRORParsingRA						= `Error when parsing RA to byte`
+	ERRORStoringRA  	  				= `Error storing RA`
 )
