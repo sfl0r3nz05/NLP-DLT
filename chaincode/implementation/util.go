f160ddfd8e4bebb02026f826c8629ccd0a9d5e00
Ubuntu <ubuntu@ip-172-31-16-137.ec2.internal>
2021-08-16 13:46:48 +0000

    16.08.21.23

--- chaincode/implementation/util.go
+++ chaincode/implementation/util.go
@@ -8,11 +8,17 @@ import (
 	"github.com/hyperledger/fabric-chaincode-go/shim"
 )
 
-func (cc *Chaincode) emitEvent(stub shim.ChaincodeStubInterface, event_name string, org string, timestamp, txid string, channelid string) (error){
+func (cc *Chaincode) emitEvent(stub shim.ChaincodeStubInterface, event_name string, org1 string, org2 string, timestamp, txid string, channelid string) (error){
+	var eventPayload string
+	
 	if event_name == "" {
 		return errors.New(ERROREventName)
 	}
-	eventPayload := "Event Name " + event_name + " organtization " + org + " timestamp " + timestamp + " TxID " + txid + " Channel ID " + channelid
+	if org2 == "" {
+		eventPayload = "Event Name " + event_name + " organtization " + org1 + " timestamp " + timestamp + " TxID " + txid + " Channel ID " + channelid
+	} else {
+		eventPayload = "Event Name " + event_name + " organtization 1 " + org1 + " organtization 2 " + org2 + " timestamp " + timestamp + " TxID " + txid + " Channel ID " + channelid
+	}
 	payloadAsBytes := []byte(eventPayload)
 	eventErr := stub.SetEvent(event_name ,payloadAsBytes)
 	if (eventErr != nil) {
