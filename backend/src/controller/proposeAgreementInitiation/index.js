8bb41f33a70dc9302b4e45b846ebd097c5d16556
Santiago Figueroa-Lorenzo <santiagofigueroalorenzo@gmail.com>
2021-10-13 11:30:13 +0000

    13.10.21.8

--- backend/src/controller/proposeAgreementInitiation/index.js
+++ backend/src/controller/proposeAgreementInitiation/index.js
@@ -5,12 +5,8 @@ const proposeAgreementInitiation = async (req, res) => {
         let data = req.body; // params from POST
         let method = "addOrg";
         let value = data.createAgreement;
-        if (data.createAgreement.mno1 != "TELEFONICA" || data.createAgreement.mno2 != "ORANGE") {
-            res.sendStatus(400);
-        } else {
-            users = await invoke(method, value);
-            res.sendStatus(200);
-        }
+        users = await invoke(method, value);
+        res.sendStatus(200);
     } catch (error) {
         console.error(error);
         res.sendStatus(400);
